c74d3aa3a82f233a8d888bdd47999d6cab5fb1e3
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-09-14 09:27:52 +0200

    Making project.clj more java-oriented.

--- project.clj
+++ project.clj
@@ -3,33 +3,29 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache License 2.0"}
 
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [cascading/cascading-hadoop "2.0.0"
+  :dependencies [[cascading/cascading-hadoop "2.0.0"
                     :exclusions [org.codehaus.janino/janino
                                  org.apache.hadoop/hadoop-core]]
-                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]
+                 [org.apache.cassandra/cassandra-all "1.1.4"]
+                 [org.apache.hadoop/hadoop-core "0.20.2-dev"]]
 
-                 [clojurewerkz/cassaforte "1.0.0-SNAPSHOT"]
-
-                 [com.twitter/maple "0.2.3"]
-                 [mysql/mysql-connector-java "5.1.18"]
-
-                 [cascalog "1.10.0"]
-
-                 [midje "1.3.0" :exclude [org.clojure/clojure]]
-                 [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]]
-
-  ;; :main com.clojurewerkz.cascading.cassandra.core
-  :source-paths   ["src/main/clj"]
+  :main com.clojurewerkz.cascading.cassandra.core
   :java-source-paths ["src/main/java"]
   :test-paths        ["src/test"]
+
+  :profiles {:dev {:resource-paths     ["src/resources"]
+                   :dependencies [[org.clojure/clojure "1.4.0"]
+                                  [clojurewerkz/cassaforte "1.0.0-SNAPSHOT"]
+                                  [cascalog "1.10.0"]
+                                  [midje "1.3.0" :exclude [org.clojure/clojure]]
+                                  [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]]}
+             }
   :test-selectors {:all     (constantly true)
                    :focus   :focus
                    :default (constantly true)}
   :aot [com.clojurewerkz.cascading.cassandra.tap com.clojurewerkz.cascading.cassandra.core]
   :javac-options     ["-target" "1.7" "-source" "1.7"]
-  :repositories {
-                 "conjars" "http://conjars.org/repo/"
+  :repositories {"conjars" "http://conjars.org/repo/"
                  "sonatype" {:url "http://oss.sonatype.org/content/repositories/releases"
                              :snapshots false
                              :releases {:checksum :fail :update :always}}
