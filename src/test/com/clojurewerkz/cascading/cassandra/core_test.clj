54745238199bdc0cb3cb52721f49be4af0441527
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-09-14 09:26:23 +0200

    Improving tests.

--- src/test/com/clojurewerkz/cascading/cassandra/core_test.clj
+++ src/test/com/clojurewerkz/cascading/cassandra/core_test.clj
@@ -1,6 +1,7 @@
 (ns com.clojurewerkz.cascading.cassandra.core-test
   (:require [clojurewerkz.cassaforte.client :as cc]
             [clojurewerkz.cassaforte.schema :as sch]
+            [clojurewerkz.cassaforte.conversion :as cconv]
             [clojurewerkz.cassaforte.cql    :as cql])
   (:use cascalog.api
         clojure.test
@@ -9,7 +10,6 @@
             [cascalog.ops :as c])
   (:import [cascading.tuple Fields]
            [cascading.scheme Scheme]
-           ;; [com.twitter.maple.jdbc JDBCTap JDBCScheme TableDesc]
            [com.clojurewerkz.cascading.cassandra CassandraTap CassandraScheme]))
 
 (defmacro with-thrift-exception-handling
@@ -19,55 +19,72 @@
    (catch org.apache.cassandra.thrift.InvalidRequestException ire#
      (println (.getWhy ire#)))))
 
-(cc/connect! "127.0.0.1" "CassaforteTest1")
+(cc/connect! "127.0.0.1" "CascadingCassandra")
 
-(deftest test-get
+(defn create-test-column-family
+  []
   (with-thrift-exception-handling
     (cql/drop-column-family "libraries"))
-
   (cql/create-column-family "libraries"
                             {:name      "varchar"
-                             :language  "varchar"}
-                            :primary-key :name)
+                             :language  "varchar"
+                             :votes     "bigint"}
+                            :primary-key :name))
 
-  (sch/create-index "libraries" :language)
-  (dotimes [n 300]
-    (cql/insert "libraries" {:name (str "Cassaforte" n) :language (str "Clojure" n)}))
+(defn create-tap
+  []
+  (let [keyspace      "CascadingCassandra"
+        column-family "libraries"
+        scheme        (CassandraScheme. "127.0.0.1"
+                                        "9160"
+                                        keyspace
+                                        column-family
+                                        "name"
+                                        (java.util.ArrayList. ["name" "language" "votes"])
+                                        (java.util.HashMap. {"name" "?value1"
+                                                             "language" "?value2"
+                                                             "votes" "?value3"}))
+        tap           (CassandraTap. scheme)]
+    tap))
 
+(deftest t-cassandra-tap-as-source
+  (create-test-column-family)
+  (dotimes [counter 100]
+    (cql/insert "libraries" {:name (str "Cassaforte" counter) :language (str "Clojure" counter) :votes counter}))
 
-  (testing ""
-    (let [scheme (CassandraScheme. "127.0.0.1" "9160" "CassaforteTest1" "libraries" (java.util.ArrayList. ["name" "language"]))
-          tap    (CassandraTap. scheme)
+  (fact "Handles simple calculations"
+        (<-
+         [?count ?sum]
+         ((create-tap) ?value1 ?value2 ?value3)
+         (c/count ?count)
+         (c/sum ?value3 :> ?sum))
+        => (produces [[100 4950]])))
 
-          test-data [["field1-1" "field1-2" "field1-3"]
-                     ["field2-1" "field2-2" "field2-3"]
-                     ["field3-1" "field3-2" "field3-3"]]]
+(deftest t-cassandra-tap-as-source-2
+  (create-test-column-family)
 
-      (?<- (stdout)
-           [?value1 ?value2 ?value3]
-           (tap ?value1 ?value2 ?value3))
-      (comment (fact ""
-                     (?<-
-                      (stdout)
-                      [?count]
-                      ((tap) ?value1 ?value2 ?value3)
-                      (c/count ?count))
-                     => (produces [[3]])
-                     )))))
+  (cql/insert "libraries" {:name "Riak" :language "Erlang" :votes 5})
+  (cql/insert "libraries" {:name "Cassaforte" :language "Clojure" :votes 3})
 
-;; (comment (defn mysql-tap []
-;;            (let [scheme (JDBCScheme. (Fields. (into-array String ["timeval" "value1" "value2" "value3"])) (into-array String ["timeval" "value1" "value2" "value3"]))
-;;                  table-desc (TableDesc. "raw_values")
-;;                  tap (JDBCTap. "jdbc:mysql://localhost:3306/eventoverse?user=root&password=root" "com.mysql.jdbc.Driver" table-desc scheme)]
-;;              tap))
+  (fact "Retrieves data"
+        (<-
+         [?value1 ?value3]
+         ((create-tap) ?value1 ?value2 ?value3))
+        => (produces [["Cassaforte" 3] ["Riak" 5]])))
 
-;;          (deftest test-get-2
-;;            (fact ""
-;;                  (?<-
-;;                   (stdout)
-;;                   [?count]
-;;                   ((mysql-tap) ?timeval ?value1 ?value2 ?value3)
-;;                   (c/count ?count))
-;;                  (produces [["field1-1" "field1-2" "field1-3"]
-;;                             ["field2-1" "field2-2" "field2-3"]
-;;                             ["field3-1" "field3-2" "field3-3"]]))))
+(deftest t-cassandra-tap-as-sink
+  (create-test-column-family)
+
+  (let [test-data [["Riak" "Erlang" 5]
+                   ["Cassaforte" "Clojure" 3]]]
+
+    (?<- (create-tap)
+         [?value1 ?value2 ?value3]
+         (test-data ?value1 ?value2 ?value3))
+    (Thread/sleep 5000)
+
+    (let [res (cconv/to-plain-hash (:rows (cql/execute "SELECT * FROM libraries")))]
+      (is (= {:name "Cassaforte" :language "Clojure" :votes 3}
+             (get res "Cassaforte")))
+      (is (= {:name "Riak" :language "Erlang" :votes 5}{:name "Riak" :language "Erlang" :votes 5}
+             (get res "Riak"))))))
