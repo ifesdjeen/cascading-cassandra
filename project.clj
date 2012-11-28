39b29cac0103663cb73416fa13afb344799ec1fe
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2012-11-28 09:49:37 +0100

    Moving hadoop-core to dev dependency. In production people should already have it, no need to bring one more version

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :min-lein-version "2.0.0"
   :license {:name "Apache License 2.0"}
 
-  :dependencies [[org.apache.cassandra/cassandra-all "1.1.5"]
-                 [org.apache.hadoop/hadoop-core "0.20.205.0"]]
+  :dependencies [[org.apache.cassandra/cassandra-all "1.1.5"]]
 
   :main com.clojurewerkz.cascading.cassandra.core
   :java-source-paths ["src/main/java"]
@@ -14,7 +13,8 @@
                                   [clojurewerkz/cassaforte "1.0.0-beta11-SNAPSHOT"]
                                   [cascalog "1.10.0"]
                                   [midje "1.3.0" :exclude [org.clojure/clojure]]
-                                  [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]]}
+                                  [midje-cascalog "0.4.0" :exclude [org.clojure/clojure]]
+                                  [org.apache.hadoop/hadoop-core "0.20.205.0"]]}
              }
   :test-selectors {:all     (constantly true)
                    :focus   :focus
