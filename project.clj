780d54b6bbc3566a41a1896e479466283efa9057
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-04-22 20:13:40 +0200

    Revert "Release 0.1.0"
    This reverts commit 5ddc6531d832235f365f82fa07307b22a46945ee.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.ifesdjeen/cascading-cassandra "0.1.0"
+(defproject com.ifesdjeen/cascading-cassandra "0.0.6-SNAPSHOT"
   :description ""
   :min-lein-version "2.0.0"
   :license {:name "Apache License 2.0"}
