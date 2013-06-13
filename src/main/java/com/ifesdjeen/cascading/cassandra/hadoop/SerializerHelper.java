14f54993112b2a524befa9c7942bc2aa5dcb5173
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-06-13 20:34:27 +0200

    extend SerializerHelper.deserialize to deserialize CompositeTypes

--- src/main/java/com/ifesdjeen/cascading/cassandra/hadoop/SerializerHelper.java
+++ src/main/java/com/ifesdjeen/cascading/cassandra/hadoop/SerializerHelper.java
@@ -5,12 +5,31 @@ import org.apache.cassandra.exceptions.ConfigurationException;
 import org.apache.cassandra.exceptions.SyntaxException;
 
 import java.nio.ByteBuffer;
+import java.util.*;
 
 public class SerializerHelper {
 
-  public static Object deserialize(ByteBuffer bb, String type) throws SyntaxException, ConfigurationException {
-    return inferType(type).compose(bb);
-  }
+    public static Object deserialize(ByteBuffer bb, String type) throws SyntaxException, ConfigurationException {
+
+        AbstractType t = inferType(type);
+
+        if (t instanceof CompositeType) {
+            CompositeType ct = (CompositeType)t;
+            List<AbstractType<?>> componentTypes = ct.types;
+            List<AbstractCompositeType.CompositeComponent> components = ct.deconstruct(bb);
+
+            ArrayList objs = new ArrayList();
+            for(int i=0 ; i < componentTypes.size() ; i++) {
+                AbstractType componentType = componentTypes.get(i);
+                Object obj = componentType.compose(components.get(i).value);
+                objs.add(obj);
+            }
+
+            return objs;
+        } else {
+            return t.compose(bb);
+        }
+    }
 
   public static AbstractType inferType(String t) throws SyntaxException, ConfigurationException {
     return org.apache.cassandra.db.marshal.TypeParser.parse(t);
