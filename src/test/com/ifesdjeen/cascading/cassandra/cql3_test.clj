f711f0d1eb87bc34004bfc581f0bf7fbdfb4b94d
mccraig mccraig of the clan mccraig <mccraigmccraig@gmail.com>
2013-08-13 17:19:23 +0100

    test for failing sinking of nil values

--- src/test/com/ifesdjeen/cascading/cassandra/cql3_test.clj
+++ src/test/com/ifesdjeen/cascading/cassandra/cql3_test.clj
@@ -227,6 +227,28 @@
       (is (= "Cassaforte" (:name (second res))))
       (is (= "Clojure" (:language (second res)))))))
 
+(deftest t-cassandra-tap-as-sink-normal-null-value
+  (let [test-data [["Riak" nil (int 100)]
+                   ["Cassaforte" "Clojure" nil]]]
+
+    (?<- (create-tap {"db.columnFamily" "libraries_cql_3"
+                      "sink.outputCQL" "UPDATE libraries_cql_3 SET votes = ?, language = ?"
+                      "mappings.cqlKeys" ["name"]
+                      "mappings.cqlValues" ["votes" "language"]
+                      "mappings.cql" {"name"     "?value1"
+                                      "language" "!value2"
+                                      "votes"    "!value3" }})
+         [?value1 !value2 !value3]
+         (test-data ?value1 !value2 !value3))
+
+    (let [res (select :libraries_cql_3)]
+      (is (= "Riak" (:name (first res))))
+      (is (= nil (:language (first res))))
+      (is (= 100 (:votes (first res))))
+      (is (= "Cassaforte" (:name (second res))))
+      (is (= "Clojure" (:language (second res))))
+      (is (= nil (:votes (second res)))))))
+
 (deftest t-cassandra-tap-as-sink-normal-composite-key
   (let [test-data [["Riak" "Erlang" (int 100)]
                    ["Cassaforte" "Clojure" (int 150)]]]
